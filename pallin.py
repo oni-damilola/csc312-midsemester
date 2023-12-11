bc797d1f6a5bad4b29e64f1721030c2fd45e85d5
oni-damilola <153301378+oni-damilola@users.noreply.github.com>
2023-12-11 17:13:50 +0100

    delete pallin.py

--- pallin.py
+++ pallin.py
@@ -1,17 +1,1 @@
-def pallin_check():
-  word = 'lawal'
-  new_word = []
-  reverse_word = []
-  m = ''
 
-  for i in range (len(word)):
-    new_word.append(i)
-
-  for i in range (len(word):
-    m = new_word.pop()
-    reverse_word.appent(m)
-    
-  if new_word == reverse_word:
-    print("Word is a pallindrome")
-  else:
-    print("Word is not a pallindrome")
